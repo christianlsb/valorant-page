06115954888a7ea1d76dd4bf53abf8a2461add15
christianlsb <chrislsb09@gmail.com>
2024-03-18 16:28:05 -0300

    Adiciona classes e eventos para o menu hamburguer e overlay

--- js/global.js
+++ js/global.js
@@ -1,4 +1,11 @@
-function toggleMenu() {
-  let overlay = document.getElementById("overlay");
-  overlay.classList.toggle("active");
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerIcon = document.querySelector(".hamburger-icon");
+  const overlay = document.querySelector(".overlay");
+
+  hamburgerIcon.addEventListener("click", function () {
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("overlay-active");
+
+    hamburgerIcon.classList.toggle("rotate");
+  });
+});
